76f7e5540e08650cc49843ee84e37614acb4f0bb
Jayden Navarro <jdndeveloper@gmail.com>
2015-10-14 22:29:21 -0700

    Converted tabs to spaces

--- src/HashTable.py
+++ src/HashTable.py
@@ -3,63 +3,63 @@
 
 class HashTable():
 
-	LOAD_THRESHOLD = 0.3
+   LOAD_THRESHOLD = 0.3
 
-	GOLDEN_VALUE = 0.618034
+   GOLDEN_VALUE = 0.618034
 
-	LOW_CAP = 10
+   LOW_CAP = 10
 
-	def __init__(self, capacity=-1):
-		self.size = 0
-		self.capacity = max(self.LOW_CAP, capacity)
-		self.table = [None] * self.capacity
+   def __init__(self, capacity=-1):
+      self.size = 0
+      self.capacity = max(self.LOW_CAP, capacity)
+      self.table = [None] * self.capacity
 
-	def loadFactor(self):
-		return self.size / self.capacity
+   def loadFactor(self):
+      return self.size / self.capacity
 
-	def resize(self):
-		oldTable = self.table
-		self.capacity *= 2
-		self.table = [None] * self.capacity
-		for bucket in oldTable:
-			if bucket != None:
-				for element in bucket:
-					self._insert(element)
+   def resize(self):
+      oldTable = self.table
+      self.capacity *= 2
+      self.table = [None] * self.capacity
+      for bucket in oldTable:
+         if bucket != None:
+            for element in bucket:
+               self._insert(element)
 
-	def insert(self, element):
-		self.size += 1
-		if self.loadFactor() > self.LOAD_THRESHOLD:
-			self.resize()
-		self._insert(element)
+   def insert(self, element):
+      self.size += 1
+      if self.loadFactor() > self.LOAD_THRESHOLD:
+         self.resize()
+      self._insert(element)
 
-	def _insert(self, element):
-		hashNum = self._hash(element)
-		if self.table[hashNum] == None:
-			self.table[hashNum] = []
-		self.table[hashNum].append(element)
+   def _insert(self, element):
+      hashNum = self._hash(element)
+      if self.table[hashNum] == None:
+         self.table[hashNum] = []
+      self.table[hashNum].append(element)
 
-	def find(self, element):
-		bucket = self.table[self._hash(element)]
-		if bucket != None:
-			if element in bucket:
-				return True
-		return False
+   def find(self, element):
+      bucket = self.table[self._hash(element)]
+      if bucket != None:
+         if element in bucket:
+            return True
+      return False
 
-	def remove(self, element):
-		self.size -= 1
-		bucket = self.table[self._hash(element)]
-		if bucket != None:
-			if element in bucket:
-				bucket.remove(element)
+   def remove(self, element):
+      self.size -= 1
+      bucket = self.table[self._hash(element)]
+      if bucket != None:
+         if element in bucket:
+            bucket.remove(element)
 
-	def _hash(self, element):
-		return int(self.capacity * (element * self.GOLDEN_VALUE - 
-			int(element * self.GOLDEN_VALUE)))
+   def _hash(self, element):
+      return int(self.capacity * (element * self.GOLDEN_VALUE - 
+         int(element * self.GOLDEN_VALUE)))
 
 HT = HashTable()
 
 for i in range(32):
-	HT.insert(i)
+   HT.insert(i)
 
 assert HT.find(23) == True
 assert HT.find(32) == False
--- src/MergeSort.py
+++ src/MergeSort.py
@@ -2,33 +2,33 @@
 # Date: 10/14/2015
 
 def mergeSort(data):
-	length = len(data)
-	if length <= 1:
-		return data
-	half = int(length / 2)
-	left = mergeSort(data[:half])
-	right = mergeSort(data[half:])
-	return merge(left, right)
+   length = len(data)
+   if length <= 1:
+      return data
+   half = int(length / 2)
+   left = mergeSort(data[:half])
+   right = mergeSort(data[half:])
+   return merge(left, right)
 
 def merge(left, right):
-	leftPos = 0
-	rightPos = 0
-	data = []
-	while leftPos < len(left) and rightPos < len(right):
-		if left[leftPos] < right[rightPos]:
-			data.append(left[leftPos])
-			leftPos += 1
-		else:
-			data.append(right[rightPos])
-			rightPos += 1
-	data.extend(left[leftPos:])
-	data.extend(right[rightPos:])
-	return data
+   leftPos = 0
+   rightPos = 0
+   data = []
+   while leftPos < len(left) and rightPos < len(right):
+      if left[leftPos] < right[rightPos]:
+         data.append(left[leftPos])
+         leftPos += 1
+      else:
+         data.append(right[rightPos])
+         rightPos += 1
+   data.extend(left[leftPos:])
+   data.extend(right[rightPos:])
+   return data
 
 if __name__ == "__main__":
-	data = [3, 5, 2, 1, 72, -23, 89, 101, -11]
-	sorted = mergeSort(data)
-	print(data)
-	print(sorted)
-	data.sort()
-	assert sorted == data
+   data = [3, 5, 2, 1, 72, -23, 89, 101, -11]
+   sorted = mergeSort(data)
+   print(data)
+   print(sorted)
+   data.sort()
+   assert sorted == data
